15e6a05f3aad082b9461e06c2bc9f4be3b99ef2f
Elijah sksksk <elijahatkins911@yahoo.com>
2023-11-12 20:28:09 -0600

    most recent changes, everything is talking to each other

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Data Doctors</title>
+	<!-- link to the stylesheet for this page -->
+    <link rel="stylesheet" href="index.css">
+</head>
 <body>
 
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
+	  <h1>My first PHP page</h1>
+	<?php
+	  require ("external_php/connect.php");
+	  $conn = OpenDb("3308");
+	  $test = $conn -> query("Select * From Department");
+	  echo "Hello World!";
+	  $counter = 1;
+	?>
+	  <button id = "test"> Hello </button>
+	<table class = "generic_table">
+		<tr> 
+			<th> ID </th>
+			<th> Dep Name </th>
+		</tr>
+		<?php while ($row = $test -> fetch_row()): ?>
+			<tr>
+				<td>
+					<?php echo $row[0] ?>
+				</td>
+				<td>
+					<?php echo $row[1] ?>
+				</td>
+			</tr>
+		<?php endwhile; ?>
+	</table>
+	<?php $test -> free_result(); ?>
 </body>
+
+<?php 
+	CloseDB($conn);
+?>
+<!-- Google's jQuery CDN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<!-- Link to the javascript file used for our page -->
+<script src="index.js"></script>
+
 </html>
