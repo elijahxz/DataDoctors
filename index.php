84a6b60579734d241160cc3575508b713fe74520
JGPadilla9 <jgpadilla09@gmail.com>
2023-11-15 16:45:47 -0600

    first page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html style = "width: 100%; height: 100%;">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -8,55 +8,34 @@
     <title>Data Doctors</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body style = "width: 100%; height: 100%;">
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-
-	<div class="text-center text-primary-emphasis">
-		<h1>My first PHP page</h1>
-	</div>
-
-
-	<?php
-	  require ("external_php/connect.php");
-	  $conn = OpenDb("3308");
-	  $test = $conn -> query("Select * From Department");
-	  echo "Hello World!";
-	  $counter = 1;
-	?>
-	  <button id = "test"> Hello </button>
-	  
-	<div class="row">
-		<div class="col-3">
+	<div class="bg-secondary" style = "width: 100%; height: 100%;">
+		<div class="row">
+			<div class="col text-center text-danger p-2 bg-dark">
+				<h1 class="fw-bold">DataDoctors</h1>
+			</div>
 		</div>
-		<div class="col-6">
-			<table class = "table table-bordered table-dark">
-				<tr> 
-					<th class="text-center"> ID </th>
-					<th class="text-center"> Dep Name </th>
-				</tr>
-				<?php while ($row = $test -> fetch_row()): ?>
-					<tr class="text-center">
-						<td>
-							<?php echo $row[0] ?>
-						</td>
-						<td>
-							<?php echo $row[1] ?>
-						</td>
-					</tr>
-				<?php endwhile; ?>
-			</table>
-		</div>
-		<div class="col-3">
+
+		<div class="row">
+			<div class="col bg-secondary">
+			<div class="row p-5">
+				<div class="col text-center p-3 d-grid">
+					<button class="btn btn-primary btn-lg p-3">CHECK-IN</button>	
+				</div>
+			</div>
+			<div class="row">
+				<div class="col d-grid justify-content-md-end">
+					<button class="btn btn-warning me-3">EMPLOYEE LOGIN</button>
+				</div>
+			</div>
+			</div>
+
 		</div>
 	</div>
-	<?php $test -> free_result(); ?>
-</body>
 
-<?php 
-	CloseDB($conn);
-?>
 <!-- Google's jQuery CDN -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
